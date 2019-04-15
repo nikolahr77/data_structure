c69117ab2d33f31615847306d7a9555a1a7e7637
Nikola Hristov <nikolahr0707@icloud.com>
2019-04-15 17:29:57 +0300

    formated code

--- sumator/main_test.go
+++ sumator/main_test.go
@@ -4,27 +4,27 @@ import "testing"
 
 func TestSumator_IntSum(t *testing.T) {
 	v := Sumator{3, -9}
-	if v.IntSum()!=-6{
-		t.Errorf("The sum is not correct, want %d, got %d",-6,v.IntSum())
+	if v.IntSum() != -6 {
+		t.Errorf("The sum is not correct, want %d, got %d", -6, v.IntSum())
 	}
 }
 
 func TestSumator_FloatSum(t *testing.T) {
 	v := Sumator{3, -9}
-	if v.FloatSum()!=-6{
-		t.Errorf("The sum is not correct, want %d, got %f",-6,v.FloatSum())
+	if v.FloatSum() != -6 {
+		t.Errorf("The sum is not correct, want %d, got %f", -6, v.FloatSum())
 	}
 }
 
 func TestSumator_StringSum(t *testing.T) {
 	v := Sumator{3, -9}
-	if v.StringSum()!="-6"{
-		t.Errorf("The sum is not correct, want %s, got %s","-6",v.StringSum())
+	if v.StringSum() != "-6" {
+		t.Errorf("The sum is not correct, want %s, got %s", "-6", v.StringSum())
 	}
 }
 
 func TestStringSum(t *testing.T) {
-	if StringSum("5", "6")!=11{
-		t.Errorf("The sum is not correct, want %d, got %d",11,StringSum("5", "6"))
+	if StringSum("5", "6") != 11 {
+		t.Errorf("The sum is not correct, want %d, got %d", 11, StringSum("5", "6"))
 	}
 }
